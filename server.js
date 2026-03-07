81dabe63e639b24ff7f6463198eb877992bff77f
DigiCoders Technologies <digicodersdevelopment@gmail.com>
2026-03-07 17:16:52 +0530

    Done

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api/gallery', require('./routes/galleryRoutes'));
 app.use('/api/settings', require('./routes/settingsRoutes'));
 
 const PORT = process.env.PORT || 5000;
-
+// ved
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
